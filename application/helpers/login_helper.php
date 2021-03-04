09904a976247d2a667d419408999b231d795510f
satria rahmat putra <satriarahmatputra@gmail.com>
2021-03-05 00:36:23 +0700

    first commit

---
+++ application/helpers/login_helper.php
@@ -0,0 +1,26 @@
+<?php
+
+
+function is_logged_in()
+{
+
+    $ci = get_instance();
+    if (!$ci->session->userdata('email')) {
+        redirect('auth');
+    } else {
+
+        $role_id = $ci->session->userdata('role_id');
+        $menu = $ci->uri->segment(1);
+        $querymenu = $ci->db->get_where('user_menu', ['menu' => $menu])->row_array();
+        $menu_id = $querymenu['id'];
+
+        $user_access = $ci->db->get_where('user_access_menu', [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+        ]);
+
+        if ($user_access->num_rows() < 1) {
+            redirect('auth/blocked');
+        }
+    }
+}
---
+++ application/views/frontend/home/index.php
@@ -0,0 +1,50 @@
+<div class="main-wrapper">
+
+    <!-- Start secvice Area -->
+
+    <div class="container">
+        <div class="row ml-3 mr-3">
+            <div class="section-top-border">
+                <center>
+                    <h3 class="mb-30">Landasan Asas Dan Prinsip Koperasi Mandani Dan Merdeka</h3>
+                </center>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <blockquote class="generic-blockquote">
+                            <p>Koperasi berlandasan pancasila dan undang-undang dasar 1945</p>
+                            <p>Koperasi berdasarkan atas asas kekeluargaan</p>
+                            <p>Koperasi melakukan kegiatannya berdasarkan prinsip koperasi yaitu<br>
+                                <a>-keanggotaan bersifat sukarela dan terbuka</a><br>
+                                <a>-pengelolaan dilakukan secara demokratis</a><br>
+                                <a>-kemandirian</a><br>
+                            </p>
+                            <p>Dalam mengembangkan koperasi, koperasi melaksanakan pula prinsip koperasi sebagai berikut:<br>
+                                <a>-pendidikan perkoperasian</a><br>
+                                <a>-kerjasama antar koperasi</a><br>
+                            </p>
+                            <p>Koperasi sebagai badan usaha dalam melaksanakan kegiatannya yang mengorganisir pemanfaatan dan pendayagunaan sumber daya ekonomi para anggotanya atas dasar prinsip-prinsip tersebut
+
+
+                            </p>
+                        </blockquote>
+                    </div>
+
+                </div>
+
+
+
+                <div class="row">
+                    <div class="col-lg-12 mt-3">
+                        <h3>Lokasi Koperasi.</h3>
+
+                        <center> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.6402660988579!2d100.43277599506597!3d-0.9407442247735732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b9d9610c9731%3A0x12a5dbfa3680811d!2sJl.%20Cupak%20Tangah%20No.29%2C%20Cupak%20Tangah%2C%20Kec.%20Pauh%2C%20Kota%20Padang%2C%20Sumatera%20Barat%2025158!5e0!3m2!1sid!2sid!4v1602353537392!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                            <center>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
